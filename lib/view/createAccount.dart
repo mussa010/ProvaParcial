8c21cc0d39092afdc903169cb756e62017c0a0e4
mussa <math_200901@hotmail.com>
2024-03-26 13:32:13 -0300

    prova parcial

---
+++ lib/view/createAccount.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateAccout extends StatefulWidget {
+  const CreateAccout({super.key});
+
+  @override
+  State<CreateAccout> createState() => _CreateAccout();
+}
+
+class _CreateAccout extends State<CreateAccout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -53,90 +55,109 @@ class _Login extends State<Login> {
             color: Colors.white,
             size: 40,
           ),
-          backgroundColor: Colors.blue[400],
+          backgroundColor: Colors.blue,
         ),
         body: SingleChildScrollView(
-            child: Padding(
-          padding: const EdgeInsets.fromLTRB(50, 100, 50, 50),
-          child: Form(
-              key: formKey,
-              child: Column(children: [
-                Image.asset('lib/imagens/login.png', width: 100, height: 100),
-                SizedBox(height: 0.03 * (MediaQuery.of(context).size.height)),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  controller: txtUsuario,
-                  decoration: InputDecoration(
-                      labelText: 'Informe seu e-mail',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      icon: const Icon(
-                        Icons.mail,
-                        color: Colors.red,
-                      )),
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Campo vazio';
-                    } else if (value.isEmpty) {
-                      return 'Campo vazio';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 0.03 * (MediaQuery.of(context).size.height)),
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  controller: txtSenha,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Informe sua senha',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      icon: const Icon(
-                        Icons.password,
-                        color: Colors.red,
-                      )),
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Campo vazio';
-                    } else if (value.isEmpty) {
-                      return 'Campo vazio';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 0.05 * (MediaQuery.of(context).size.height)),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    minimumSize: const Size(200, 60),
-                    backgroundColor: Colors.blue[400],
-                    foregroundColor: Colors.white,
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                  color: Colors.blue, 
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40)),
                   ),
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      //Validação com sucesso
-                      setState(() {
-                        var txt1 = txtUsuario.text;
-                        var txt2 = txtSenha.text;
-
-                        var msg = 'Usuario: $txt1\nSenha: $txt2';
-                        caixaDialogo(context, "Usuário e senha", msg);
-                      });
-                    }
-                  },
-                  child: const Text('Login'),
-                ),
-                SizedBox(height: 0.03 * (MediaQuery.of(context).size.height)),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    minimumSize: const Size(200, 60),
-                    backgroundColor: Colors.blue[400],
-                    foregroundColor: Colors.white,
+                  child: Container(
+                    color: Colors.blue,
+                    child: Image.asset('lib/imagens/login.png',
+                    
+                    ),
                   ),
-                  onPressed: () {},
-                  child: const Text('Criar conta'),
                 ),
-              ])),
-        )));
+                Padding(
+                          padding: const EdgeInsets.fromLTRB(50, 100, 50, 50),
+                          child: Form(
+                  key: formKey,
+                  child: Column(children: [
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: txtUsuario,
+                      decoration: InputDecoration(
+                          labelText: 'Informe seu e-mail',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          icon: const Icon(
+                            Icons.mail,
+                            color: Colors.red,
+                          )),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Campo vazio';
+                        } else if (value.isEmpty) {
+                          return 'Campo vazio';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 0.03 * (MediaQuery.of(context).size.height)),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: txtSenha,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: 'Informe sua senha',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          icon: const Icon(
+                            Icons.password,
+                            color: Colors.red,
+                          )),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Campo vazio';
+                        } else if (value.isEmpty) {
+                          return 'Campo vazio';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 0.05 * (MediaQuery.of(context).size.height)),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(200, 60),
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          //Validação com sucesso
+                          setState(() {
+                            var txt1 = txtUsuario.text;
+                            var txt2 = txtSenha.text;
+                
+                            var msg = 'Usuario: $txt1\nSenha: $txt2';
+                            caixaDialogo(context, "Usuário e senha", msg);
+                          });
+                        }
+                      },
+                      child: const Text('Login'),
+                    ),
+                    SizedBox(height: 0.03 * (MediaQuery.of(context).size.height)),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(200, 60),
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        // Navigator.push(context, )
+                
+                      },
+                      child: const Text('Criar conta'),
+                    ),
+                  ])),
+                        ),
+              ],
+            )));
   }
 }
