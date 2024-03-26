129ec8076466784127da4ed25c4f19ba4213e9f4
mussa <math_200901@hotmail.com>
2024-03-26 13:40:34 -0300

    prova parcial

--- lib/view/createAccount.dart
+++ lib/view/createAccount.dart
@@ -10,6 +10,23 @@ class CreateAccout extends StatefulWidget {
 class _CreateAccout extends State<CreateAccout> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Tela de cadastro',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: const Icon(
+            Icons.account_circle_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          backgroundColor: Colors.blue,
+        ),
+    );
   }
 }
